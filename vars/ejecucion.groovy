7447539c976f5a45daca5615bfd31fa8f93ec19b
Jorge Arias <79316244+jorge-arias@users.noreply.github.com>
2022-02-06 19:20:44 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -41,12 +41,12 @@ pipeline {
           success {
             //figlet 'pipeline:'+ PIPELINE
             //[${PIPELINE}][Rama: ${GIT_BRANCH}][Stage: ${STAGE}][Resultado: Ok]
-            slackSend color: 'good', message: "[Grupo6][Rama: ${GIT_BRANCH}][Stage: ${STAGE}][Resultado: Ok]"
+            slackSend color: 'good', message: "[Grupo6][Rama: ${env.GIT_BRANCH}][Stage: ][Resultado: Ok]"
           }
 
           failure {
             //[${PIPELINE}][Rama: ${GIT_BRANCH}][Stage: ${STAGE}][Resultado: No Ok]
-            slackSend color: 'danger', message: "[Grupo6][Rama: ${GIT_BRANCH}][Stage: ${STAGE}][Resultado: No Ok]"
+            slackSend color: 'danger', message: "[Grupo6][Rama: ${env.GIT_BRANCH}][Stage: ][Resultado: No Ok]"
             error "Ejecución fallida en stage ${STAGE}"
           }
         }
